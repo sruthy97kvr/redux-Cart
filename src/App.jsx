1e2ac2b30506b90b6f71f2f74abf1022252456ee
Sruthy VS <sruthy97kvr@gmail.com>
2025-05-28 11:24:50 +0530

    Redux-cart

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 
 import './App.css'
 import { Routes,Route } from 'react-router-dom'
-import Home from './pages/home'
+import Home from './pages/Home1'
 import Cart from './pages/cart'
 import WhishList from './pages/WhishList'
 import View from './pages/View'
